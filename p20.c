1ffb3d694622409a8c825950367be4f31dcb1030
Tanisha Kothadia <tanishakothadia@yahoo.com>
2025-11-21 01:02:31 +0530

    Add files via upload

---
+++ p20.c
@@ -0,0 +1,29 @@
+/*AIM:Tracking Attendence
+PROGRAMER:Tanisha Kothadia
+LAST UPDATED:12-09-2025
+*/
+#include<stdio.h>
+void main()
+{
+    int rs[25],as[25],i;
+    for(i=0;i<25;i++)
+    {
+        printf("enter the registered ID of participant:");
+        scanf("%d",&rs[i]);
+    }
+    for( i=0;i<24;i++)
+    {
+        printf("participants who attended are:");
+        scanf("%d",&as[i]);
+    }
+    for(i=0;i<25;i++)
+    {
+        if(rs[i]!=as[i])
+        {
+            printf("absent participant is :%d",rs[i]);
+            break;
+        }
+
+    }
+}
+
